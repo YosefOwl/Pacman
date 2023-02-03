06c856661815952524c8ad47c41be64f1ffb3e1c
LiorAriely <lior.ariely@gmail.com>
2023-02-03 13:30:14 +0200

    Little change in welcome state

--- GamePacman/src/WelcomeState.java
+++ GamePacman/src/WelcomeState.java
@@ -4,6 +4,7 @@ import java.awt.event.KeyEvent;
 public class WelcomeState extends GameState {
 
 	private boolean active;
+	public int numState = 0;
 
 	public void enter(Object memento) {
 		active = true;
@@ -11,11 +12,25 @@ public class WelcomeState extends GameState {
 
 	public void processKeyReleased(int aKeyCode) {
 
-		if (aKeyCode == KeyEvent.VK_ESCAPE)
-			System.exit(0);
-		active = false;
-	}
+		if (aKeyCode == KeyEvent.VK_DOWN) {
+			numState++;
+			if (numState > 1)
+				numState = 0;
+		}
 
+		if (aKeyCode == KeyEvent.VK_UP) {
+			numState--;
+			if (numState < 0)
+				numState = 1;
+		}
+
+		if (aKeyCode == KeyEvent.VK_ENTER) {
+			if (numState == 0)
+				active = false;
+			else
+				System.exit(0);
+		}
+	}
 	public boolean isActive() { return active; }
 
 	public String next() {
@@ -25,11 +40,38 @@ public class WelcomeState extends GameState {
 	public void render(GameFrameBuffer aGameFrameBuffer) {
 
 		Graphics g = aGameFrameBuffer.graphics();
-		String text = "PRESS ANY KEY TO PLAY";
-		int textWidth = g.getFontMetrics().stringWidth(text);
+		String text = "WELCOME";
+		Font font = new Font("Serif", Font.BOLD, 88);
+		g.setFont(font);
+		//Shadow
+		g.setColor(Color.pink);
+		g.drawString(text, (Game.WIDTH/3-140+2), Game.HEIGHT/2+2);
+		//Title
+		g.setColor(Color.red);
+		g.drawString(text, (Game.WIDTH/3-140), Game.HEIGHT/2);
+
+		g.setColor(Color.yellow);
+		g.drawOval(280, 300, 60, 60);
+		g.fillOval(280, 300, 60, 60);
+
+		//Menu
+		String text2 = "START GAME";
+		g.setFont(new Font("Serif", Font.BOLD, 30));
 		g.setColor(Color.white);
-		g.drawString(text, (aGameFrameBuffer.getWidth()-textWidth)/2, aGameFrameBuffer.getHeight()/2);
+		g.drawString(text2, 210, 420);
+		if(numState==0){
+			g.drawString(">", 185, 420 );
+		}
+
+		String text3 = "EXIT";
+		g.setFont(new Font("Serif", Font.BOLD, 30));
+		g.setColor(Color.white);
+		g.drawString(text3, 270, 465);
+		if(numState==1){
+			g.drawString(">", 245, 465 );
+		}
 	}
+
 
 	public Object memento() {
 		return this;
